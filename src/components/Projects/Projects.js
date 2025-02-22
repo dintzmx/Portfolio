14bc845e04edc5c18ca1b26b11a7de8a558cfdcf
dintzmx <david.s.lee1998@gmail.com>
2025-02-21 21:02:50 -0600

    Sean name update
    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -106,7 +106,7 @@ function Projects() {
                 <>
                   Directors: Victor Garcés & Kronovox<br />
                   <span style={{ marginLeft: "0px" }}>DP: Victor Garcés</span><br />
-                  <span style={{ marginLeft: "0px" }}>CLT: Sean Dagiwa</span><br />
+                  <span style={{ marginLeft: "0px" }}>CLT: Sean Altuna</span><br />
                   <span style={{ marginLeft: "0px" }}>Role: Dimmer Board Operator</span>
                 </>
                }
